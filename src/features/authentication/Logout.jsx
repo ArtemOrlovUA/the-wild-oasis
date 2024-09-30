779f122990daec0457b42854b341a0233924b89e
ArtemOrlovUA <artemorel200415@gmail.com>
2024-09-30 16:52:58 +0300

    fix cache bug in authorization

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightEndOnRectangle } from 'react-icons/hi2';
 import ButtonIcon from '../../ui/ButtonIcon';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 function Logout() {
+  const { logout, isLoggingOut } = useLogout();
+
   return (
-    <ButtonIcon>
-      <HiArrowRightEndOnRectangle />
+    <ButtonIcon onClick={logout} disabled={isLoggingOut}>
+      {!isLoggingOut ? <HiArrowRightEndOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
   );
 }
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -4,14 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export function useLogin() {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoadingAuth } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      navigate('/dashboard');
-      queryClient.setQueriesData(['user'], user);
+      console.log(user);
+      navigate('/dashboard', { replace: true });
+      queryClient.setQueryData(['user'], user.user);
     },
     onError: (error) => {
       console.error(error);
